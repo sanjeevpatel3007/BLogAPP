c8be82942990ee82c07f79aebee12a3a61a41ba6
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-10-03 11:55:36 +0530

    add link

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,6 +1,6 @@
 // utils/constant.js
 
-export const API_ROOT = "http://localhost:4000/api/v1";
+export const API_ROOT = "https://blogapp-backend-q5ks.onrender.com/api/v1";
 
 // Define specific API endpoints
 
